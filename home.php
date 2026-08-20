bfffd3594b72898e57623245ebc3404b83109534
8tep/jithubmaster <zachary.lemmon@my.rongotai.school.nz>
2026-08-20 23:54:56 +0000

    commit ts yo

--- home.php
+++ home.php
@@ -8,5 +8,7 @@ include('includes/nav.php');
 ?>
 <h1 class="pt-5 pb-4 text-center">Home Page</h1>
 <p class="text-center"> Welcome to the Book Drop home page! </p>
-<a href="view_loans.php"><img src="https://img.magnific.com/free-photo/glad-young-man-resting-cafe-reading-interesting-book-drinking-tea_8353-6242.jpg?semt=ais_hybrid&w=740&q=80" class="img-fluid mx-auto d-block" alt="View Loans"></a>
+<a href="view_loans.php">
+<img src="https://img.magnific.com/free-photo/glad-young-man-resting-cafe-reading-interesting-book-drinking-tea_8353-6242.jpg?semt=ais_hybrid&w=740&q=80" class="img-fluid mx-auto d-block" alt="View Loans" width = "500" height ="500"></a>
+</a>
 <?php include('includes/footer.php'); ?>
